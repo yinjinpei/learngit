037754dca9f19b6fe8865700953fa57e35908763
peter <455758882@qq.com>
2018-06-18 15:01:33 +0800

    例子：用户输入不是数字则重新输入，提示输入必须数字。

--- oneDay.py
+++ oneDay.py
@@ -1,4 +1,22 @@
-#!/bin/bash/env python
+#!user/bin/env python
 #_*_ coding:utf-8 _*_
 
-print('asdfasdf')
+name = input('Plesase input your name:')
+while True:
+    try:
+        age = int(input('Plesase input your age:'))
+        break
+    except ValueError:
+        print('ERROR: Value of initial position must be int number!')
+
+job = input('Plesase input your job:')
+salary = input('Plesase input your salary:')
+
+print ('''
+Personal information of %s:
+        Name: %s
+        Age:  %s   
+        Job:  %s
+     Salary:  %s
+--------------------------      
+''' % (name,name,age,job,salary))
