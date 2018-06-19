66df504859dc7d811ad70895737f9e262153bc3f
peter <ex_wljr_yinjinpei@pingan.com.cn>
2018-06-19 19:22:54 +0800

    修正while

--- whlie.py
+++ whlie.py
@@ -14,10 +14,10 @@ while count < 1000000:
     if count == print_num:
         print('两个数字相等！！', '数字为：', count)
         str_1 = input('是否继续输入？ Y/N :   ')
-        print(type(str_1))
+        #print(type(str_1))
 
         if str_1 == 'y':
-            print('继续输入！！！')
+            #print('继续输入！！！')
             while True:
                 try:
                     print_num = int(input('请输入一个数字： '))
@@ -28,7 +28,7 @@ while count < 1000000:
                 except ValueError:
                     print('输入错误，请输入一个正整数!!')
         else:
-            print('放弃！！！')
+            print('结束输入！！！')
             break
     else:
         count += 1
