211bc3fd7373f3ad3d4bca3c598550361610a4fc
peter <ex_wljr_yinjinpei@pingan.com.cn>
2018-06-19 12:15:22 +0800

    添加whlie

---
+++ whlie.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#_*_ coding:utf-8 _*_
+
+
+count = 0
+while True:    try:
+        print_num = int(input('请输入一个数字： '))
+        break
+    except ValueError:
+        print('输入错误，请输入一个正整数!!')
+
+while count < 1000000:
+    if count == print_num:
+        print('两个数字相等！！', '数字为：', count)
+        str_1 = input('是否继续输入？ Y/N')
+        print(type(str_1))
+        if str_1 == 'y':
+            print('YYYYYY')
+        else:
+            print('放弃！！！')
+    else:
+        count += 1
+        print(count)
