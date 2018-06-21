fc066f079a93d0e31602eb0f44e741e1f2415048
peter <ex_wljr_yinjinpei@pingan.com.cn>
2018-06-22 06:50:14 +0800

    文件处理

---
+++ 文件处理/readline.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#_*_ coding:utf-8 _*_
+
+
+f = open("readline_file.txt",'r')
+while True:
+    line = f.readlines()      #对应内容一
+    #line = f.readline()      #对应内容二
+    if line:
+        print('内容如下：')
+
+        #内容一 ，与readlines使用
+        data = line[1]
+        print(data.split(':')[0])
+        print(data.split(':')[1].strip())   #split() 设置分割符    ;strip() 去空行
+        print(data.strip('\n').split(':'))  #strip('\n') 去除换行符
+
+        #内容二 ，与readline使用
+        #print(line.strip().split(':')[0])
+        #print(line.strip().split(':'))
+
+        pass
+    else:
+        break
+print('结束')
+f.close()
---
+++ 文件处理/write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#_*_ coding:utf-8 _*_
+
+
+f = open('write_passwdFile.txt','r')
+for i in f.readlines():
+f.close()
+
+f = open("write_file.txt",'w')
+f.write('hihi')     #以覆盖形式写入
+f.write('sksk')
+f.write('\nggggg')  #\n 表示换行后再写入
+f.close()
