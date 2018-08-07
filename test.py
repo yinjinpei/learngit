63babceca9d634b8e8c7d55bb256a2996708c467
peter <455758882@qq.com>
2018-08-07 21:25:30 +0800

    列表去重

--- test.py
+++ test.py
@@ -12,3 +12,11 @@ movieName.remove('指环王')
 print('------删除之后------')
 for tempName in movieName:
     print(tempName)
+
+#列表去重
+ids = [1,2,3,3,4,2,3,4,5,6,1]
+news_ids = []
+for id in ids:
+    if id not in news_ids:
+        news_ids.append(id)
+print (news_ids)
