831e1ae82091ef1fab6580ca1de16763ac902f1a
peter <455758882@qq.com>
2018-08-19 15:37:56 +0800

    01-模块重加载导入

---
+++ python就业班/第2章 python核心编程/第1节.python核心编程/01.系统编程-进程/01.python高级1/01-模块重新导入_recv.py
@@ -0,0 +1,38 @@
+
+import sys
+from imp import *   #重加载模块reload所用的模块
+import test
+
+print(sys.path)  #显示当前搜索的模块路径
+
+sys.path.append("../") #添加搜索模块路径
+
+print(sys.path)
+
+reload(test)    #重加载模块
+
+
+
+'''
+ ========= 循环导入问题 ===========
+a.py:
+    from b import b
+    
+    print ("-----a ------")
+
+
+b.py:
+    from a import a
+    
+    print ("-----b ------")
+
+==================================
+
+=========== 解决方法 ==============
+#a,b都不导入模块，新建c.py:
+    from a import a
+    from b import b
+    a()
+    b()
+
+'''
