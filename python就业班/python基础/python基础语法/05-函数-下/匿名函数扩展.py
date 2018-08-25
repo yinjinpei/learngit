6670ea352715a423f5b0a7b587ed9653732314eb
peter <455758882@qq.com>
2018-08-26 06:39:10 +0800

    匿名函数扩展

--- python就业班/python基础/python基础语法/05-函数-下/匿名函数扩展.py
+++ python就业班/python基础/python基础语法/05-函数-下/匿名函数扩展.py
@@ -3,8 +3,8 @@ def test(a,b,func):
     result = func(a,b)
     return result
 
-func_new = input("请输入一个函数: ")
-func_new =eval(func_new)
+func_new = input("请输入一个函数: ")   #输入：lambda x,y:x+y
+func_new =eval(func_new)    #eval函数就是实现list、dict、tuple与str之间的转化，相当于把""符号去除
 num = test(11,22,func_new)
 print(num)
 
