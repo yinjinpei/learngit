c2e52d2e6dce9d4b48711e399e77038caed58c5d
peter <455758882@qq.com>
2018-09-04 09:08:43 +0800

    test

--- test2.py
+++ test2.py
@@ -18,18 +18,83 @@
 # 列表：%r，
 # 字典：%r，
 # 元组：%r """%(a,b,c,d))
+# #
 #
+# import time,datetime
+#
+# #设置发送时间（年, 月, 日, 时, 分, 秒）
+# startTime = datetime.datetime(2018, 8, 30, 15, 23, 0)
+#
+# while True:
+#     if startTime < datetime.datetime.now():
+#         print("现在时间是:")
+#         print(datetime.datetime.now())
+#         break
+#     else:
+#         time.sleep(1)
+#         print("时间还没到！ %s"%datetime.datetime.now())
+# def test(num):
+#     print("-"*30)
+#     def test2():
+#         print("test2")
+#     test2()
+#     print(num)
+#
+# test(20)
+#
+#
+# a = "my name is peter"
+# b = [1,3,4,6,7,8]
+# c = {"name":"peter","age":18}
+# d = (11,33,66)
+#
+# print("""
+# 字串符：%r，
+# 列表：%r，
+# 字典：%r，
+# 元组：%r """%(a,b,c,d))
+#
+#
+# import time,datetime
+#
+# #设置发送时间（年, 月, 日, 时, 分, 秒）
+# startTime = datetime.datetime(2018, 8, 30, 15, 23, 0)
+#
+# while True:
+#     if startTime < datetime.datetime.now():
+#         print("现在时间是:")
+#         print(datetime.datetime.now())
+#         break
+#     else:
+#         time.sleep(1)
+#         print("时间还没到！ %s"%datetime.datetime.now())
 
+
+
+from threading import Timer
+from wxpy import  *
 import time,datetime
+import itchat
 
-#设置发送时间（年, 月, 日, 时, 分, 秒）
-startTime = datetime.datetime(2018, 8, 30, 15, 23, 0)
+itchat.auto_login(hotReload=True)
 
-while True:
-    if startTime < datetime.datetime.now():
-        print("现在时间是:")
-        print(datetime.datetime.now())
-        break
-    else:
-        time.sleep(1)
-        print("时间还没到！ %s"%datetime.datetime.now())
+# def send_msg(msg,gname):
+#     rooms = itchat.get_chatrooms(update=True)
+#     if rooms is not None:
+#         print(rooms)
+#     else:
+#         print("空的")
+
+rooms = itchat.get_chatrooms(update=True)
+#rooms = itchat.search_chatrooms(name="紫川")
+if rooms is not None:
+    print(len(rooms))
+    print(rooms)
+    for i in rooms:
+        #print(type(i))
+        #print(type(i['MemberList']))
+        for j in i['MemberList']:
+            print(j["NickName"])
+
+else:
+    print("空的")
---
+++ 微信群发4.py
@@ -0,0 +1,56 @@
+#-*- coding:utf-8 -*-
+#author:YJ沛
+
+from __future__ import unicode_literals
+from threading import Timer
+from wxpy import  *
+import time,datetime
+
+# 生成登录二维码
+bot = Bot()
+dateTime = None
+group = bot.groups().search('兄弟')[0]
+
+def send_news():
+    try:
+        # 添加需要接收消息的人的昵称，注意是写的是微信昵称，非备注名，也不是微信号，可添加多个
+        #nickname = ['微信昵称1','微信昵称2','微信昵称3']
+
+        friends = []
+        #for i in nickname:
+        #    friends.append(bot.friends().search(i)[0])
+
+        for i in group:
+            friends.append(bot.friends().search(i)[0])
+
+
+        # 设置发送的内容
+        for friend in friends:
+            friend.send('----for test----')
+        print("消息已发送，发送时间: %s" % dateTime)
+
+        #重复发送(骚扰功能)，设定时间间隔60秒，此功能默认不开启
+        #t = Timer(60,send_news)
+        #t.start()
+    except:
+        print("发送失败。。。")
+
+def set_time():
+    # 设置发送时间
+    # 如设置在当前时间之前，将即时发送（年, 月, 日, 时, 分, 秒）
+    startTime = datetime.datetime(2018, 8, 30, 17, 14, 0)
+    while True:
+        if startTime < datetime.datetime.now():
+            break
+        else:
+            time.sleep(1)
+            print("时间没到,当前时间：%s" % datetime.datetime.now())
+
+def main():
+    set_time()
+    global dateTime
+    dateTime = datetime.datetime.now()
+    send_news()
+
+if __name__ == "__main__":
+    main()
