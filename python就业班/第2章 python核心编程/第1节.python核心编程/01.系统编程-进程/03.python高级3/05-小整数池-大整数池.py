00ec96f3b22509b9133565308cd068de25dba4c5
peter <455758882@qq.com>
2018-09-07 23:33:25 +0800

    03-元类-type创建类和动态创建类

---
+++ python就业班/第2章 python核心编程/第1节.python核心编程/01.系统编程-进程/03.python高级3/05-小整数池-大整数池.py
@@ -0,0 +1,51 @@
+#-*- coding:utf-8 -*-
+#author:YJ沛
+
+'''
+以下是针对python2，python3都一样，id地址不变
+小整数池：[-5 - 256] 这些字数系统已创建好了，不会被垃圾回收
+大整数池：只有被创建时才会创建，可以被垃圾回收
+intern机制：字符串共用一份，id地址一样
+
+'''
+#小整数池：[-5 - 256]
+a = 100
+b = 100
+print(id(a))
+print(id(b))
+
+del a   #回收
+del b
+
+a1 = 100
+b1 = 100
+print(id(a1))   #结果和上面一样，同一地址，说明没有被回收掉
+print(id(b1))
+
+
+#大整数池
+c = 10000
+d = 10000
+print(id(c))
+print(id(d))
+
+del c   #回收
+del d
+
+c1 = 10000
+d1 = 10000
+print(id(c1))   #结果和上面不一样，不同的地址，说明被回收掉了
+print(id(d1))
+
+
+#intern机制，字符串共用一份，id地址一样，如:
+a = 'peter'
+b = 'peter'
+print(id(a))
+print(id(b))
+
+del a
+del b
+c = 'peter'
+print(id(c))    #都一样?  可能是同一进程（同一程序），没有被回收掉？
+
