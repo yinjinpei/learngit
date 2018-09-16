c151b4c4c8ca87833a1e01d55524ba8e4c3dcffa
peter <455758882@qq.com>
2018-09-16 08:39:20 +0800

    更改目录

--- test6.py
+++ test6.py
@@ -1,32 +1,12 @@
 #-*- coding:utf-8 -*-
 #author:YJ沛
-
-
+from threading import Timer,Thread
+import threading
 def test():
-    global a
-    a = 100
-    print(a)
+    print(' The Tread name is %s ---------'%threading.current_thread().name)
+    t = Timer(2, test)
+    t.start()
 
-test()
-print(a)
-
-import random
-
-print(random.random())
-
-
-class NameError(Exception):
-    print('11111111111')
-
-
-try:
-    s = None
-    if s is None:
-        print("s 是空对象")
-
-        raise NameError  # 如果引发NameError异常，后面的代码将不能执行
-
-except NameError:
-    print('1111111111111111111')
-except TypeError:
-    print("空对象没有长度")
+# test()
+t = Timer(2, test)
+t.start()
