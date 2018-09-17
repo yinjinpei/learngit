96d1d40b960d2a901f95bc68acedf248ab16501d
peter <455758882@qq.com>
2018-09-18 05:57:39 +0800

    高级-更改目录

--- python学习/第2章 python核心编程/第3节 网络编程/01.网络编程概述、SOCKET/02-使用socket发送数据.py
+++ python学习/第2章 python核心编程/第3节 网络编程/01.网络编程概述、SOCKET/02-使用socket发送数据.py
@@ -1,4 +1,4 @@
-#-*- coding:utf-8 -*-
+
 #author:YJ沛
 
 from socket import *
@@ -7,15 +7,12 @@ from socket import *
 udpSocker = socket(AF_INET, SOCK_DGRAM)
 
 # 做准备接收方的地址
-sendAddr = ('192.168.0.10', 8080)
-
+sendIP = input("请输入接收人的IP地址：")
+sendPort = int(input("请输入接收人的端口号："))
 # 输入发送内容
 sendMsg = input('请输入要发送的数据：')
 
 # 发送内容
-udpSocker.sendto(sendMsg.encode(encoding='utf8'),sendAddr)
-
-#udpSocker.sendto(sendMsg.encode(encoding='gb2312'),sendAddr)
-
+udpSocker.sendto(sendMsg.encode('utf-8'),(sendIP,sendPort))
 
 udpSocker.close()
