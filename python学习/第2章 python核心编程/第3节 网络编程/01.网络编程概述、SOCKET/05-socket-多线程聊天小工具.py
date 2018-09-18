69d7a7067f9c8949e65b140b57a408c580b57154
peter <455758882@qq.com>
2018-09-18 21:21:35 +0800

    聊天工具

--- python学习/第2章 python核心编程/第3节 网络编程/01.网络编程概述、SOCKET/05-socket-多线程聊天小工具.py
+++ python学习/第2章 python核心编程/第3节 网络编程/01.网络编程概述、SOCKET/05-socket-多线程聊天小工具.py
@@ -3,37 +3,33 @@
 
 from socket import *
 from threading import Thread
-import time
+
 
 # 创建一个套接字
 udpSocker = socket(AF_INET, SOCK_DGRAM)
 
 def recvData():
     # 设置地址和端口
-    bindAddr = ('', 7788)   # ip地址和端⼝号，ip⼀般不⽤写，表示本机的任何⼀一个
-
+    bindAddr = ('', 7788)   # ip地址和端口号，ip一般不填写，表示本机的任何一个
     # 绑定地址和端口
     udpSocker.bind(bindAddr)
 
     while True:
         # 等待接收内容
-        recvMsg = udpSocker.recvfrom(1024)  # 1024表示本次接收的最⼤字节数
-
-        # 显示对⽅发送的数据
-        print('收到内容：\n',recvMsg[0].decode('gb2312'))
-
-        time.sleep(0.5)
+        recvMsg = udpSocker.recvfrom(1024)  # 1024表示本次接收的最大字节数
+        # 显示接收内容
+        print('\r收到内容: %s: %s '%(recvMsg[1],recvMsg[0].decode('gb2312')))
+        print('\r请输入发送内容：',end='')
 
 
 def sendData():
     # 设置接收方IP和端口
-    sendAddr = ('192.168.0.10', 8080)
+    sendAddr = ('192.168.0.100', 8080)
 
     while True:
         # 设置发送内容
-        sendMsg = input("\n请输入发送内容：")
-
-        # 发送
+        sendMsg = input("\r请输入发送内容：")
+        # 发送消息
         udpSocker.sendto(sendMsg.encode('gb2312'), sendAddr)
 
 
---
+++ test6.py
@@ -0,0 +1,5 @@
+
+
+while True:
+    a = int(input('输入！！！！'))
+    print('\r copy 的进度是：%.2f%%'%(a*100),end='')
