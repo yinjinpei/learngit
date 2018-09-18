1cc9945347119d31effaf0889a26227ad881ba19
peter <ex_wljr_yinjinpei@pingan.com.cn>
2018-09-18 18:09:05 +0800

    udp聊天工具

---
+++ python学习/第2章 python核心编程/第3节 网络编程/01.网络编程概述、SOCKET/06-socket聊天小工具.py
@@ -0,0 +1,49 @@
+# -*- coding:utf-8 -*-
+# author:YJ沛
+
+
+from socket import *
+from threading import Thread
+import time
+
+# 创建一个套接字
+udpSocker = socket(AF_INET, SOCK_DGRAM)
+
+def recvData():
+    # 设置地址和端口
+    bindAddr = ('', 7788)  # ip地址和端⼝号，ip⼀般不⽤写，表示本机的任何⼀一个
+    # 绑定地址和端口
+    udpSocker.bind(bindAddr)
+    while True:
+        # 等待接收内容
+        recvMsg = udpSocker.recvfrom(1024)  # 1024表示本次接收的最⼤字节数
+        # 显示对⽅发送的数据
+        print('收到内容：\n', recvMsg[0].decode('gb2312'))
+        str = input('请输入发送内容：')
+        udpSocker.sendto(str.encode('gb2312'),recvMsg[1])
+        time.sleep(0.5)
+
+def sendData():
+    # 设置接收方IP和端口
+    sendAddr = ('172.19.11.122', 8080)
+    while True:
+        # 设置发送内容
+        sendMsg = input("\n请输入发送内容：")
+        # 发送
+        udpSocker.sendto(sendMsg.encode('gb2312'), sendAddr)
+
+
+def main():
+
+    t = Thread(target=recvData)
+    t.start()
+
+    # t2 = Thread(target=sendData)
+    # t2.start()
+
+    # 关闭套接字
+    # udpSocker.close()
+
+
+if __name__ == "__main__":
+    main()
