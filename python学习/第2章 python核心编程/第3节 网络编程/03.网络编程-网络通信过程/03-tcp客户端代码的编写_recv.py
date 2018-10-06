e0c9437eb0f9e02535b901211de57faa0fa9b740
peter <455758882@qq.com>
2018-10-06 10:07:06 +0800

    02-tcp客户端

--- python学习/第2章 python核心编程/第3节 网络编程/03.网络编程-网络通信过程/03-tcp客户端代码的编写_recv.py
+++ python学习/第2章 python核心编程/第3节 网络编程/03.网络编程-网络通信过程/03-tcp客户端代码的编写_recv.py
@@ -13,14 +13,19 @@
 
 from socket import *
 
+#1，socket创建一个套接字
 clientSocket = socket(AF_INET, SOCK_STREAM)
 
+#2，econnect连接服务器
 clientSocket.connect(('192.168.0.7',8989))
 
+#3，send发送数据
 clientSocket.send("haha_hehe".encode('gb2312'))
 
+#4，recv接收数据
 recvData = clientSocket.recv(1024)
 
 print(recvData.decode('gb2312'))
 
+#5，close关闭套接字
 clientSocket.close()
