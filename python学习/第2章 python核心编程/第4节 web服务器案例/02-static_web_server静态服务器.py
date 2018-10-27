5b4ce74dc8a9c2c26de2822cc504db1c1dd1a7fa
peter <455758882@qq.com>
2018-10-27 22:48:47 +0800

    重命名

--- python学习/第2章 python核心编程/第4节 web服务器案例/02-static_web_server静态服务器.py
+++ python学习/第2章 python核心编程/第4节 web服务器案例/02-static_web_server静态服务器.py
@@ -1,50 +1,48 @@
-#-*- coding:utf-8 -*-
-#author:YJ沛
+# -*- coding:utf-8 -*-
+# author:YJ沛
 
 
 from socket import *
 from multiprocessing import Process
 
 
-
 def response_client(client_socket, client_data):
     try:
         while True:
-            recv_Data = client_socket.recv(1024)
-            if recv_Data:
-                print("来自%s：\n%s"%(client_data,recv_Data.decode('gb2312')))
+            recv_data = client_socket.recv(1024)
 
-
+            if recv_data:
+                print("来自%s：\n%s" % (client_data, recv_data.decode('gb2312')))
                 client_socket.send("你好！！".encode('gb2312'))
             else:
                 break
 
     except EOFError:
         pass
-
     # 关闭套接字
     client_socket.close()
 
 
 def main():
     # 1，创建server_socket
-    server_socket = socket(AF_INET,SOCK_STREAM)
+    server_socket = socket(AF_INET, SOCK_STREAM)
 
     # 解决程序占用端口问题（服务器先结束则会出现端口被占用）
     server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 
-    local_ip_and_port = ("",7788)
+    local_ip_and_port = ("", 7788)
     server_socket.bind(local_ip_and_port)
     server_socket.listen(100)
 
     # 2,连接客户端
     while True:
         client_socket, client_data = server_socket.accept()
-        P = Process(target=response_client, args=(client_socket, client_data))
-        P.start()
+        p = Process(target=response_client, args=(client_socket, client_data))
+        p.start()
 
         # 因为已经向子进程中copy了一份（引用），并且父进程中这个套接字也没有用处了,所有关闭
         client_socket.close()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
