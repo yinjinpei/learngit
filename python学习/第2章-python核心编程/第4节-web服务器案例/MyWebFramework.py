0fbd9e0ff108f1a5380b4595846b83c91f3f1fbb
peter <455758882@qq.com>
2018-11-03 12:49:21 +0800

    MyWebFramework.py, MyWebServer.py

---
+++ python学习/第2章-python核心编程/第4节-web服务器案例/MyWebFramework.py
@@ -0,0 +1,58 @@
+# coding:utf-8
+# author:YJ沛
+# 07-web框架-my_web_framework.py
+
+import time
+from MyWebServer import HttpServer
+
+class Application(object):
+    '''框架的核心部分， 也就是框架的主题程序，框架是通过的'''
+    def __init__(self, urls):
+        self.urls = urls
+
+    def __call__(self, env, start_response):
+
+        # 查找env字典中有没有“Content-Type”，如果没有默认设定为“/”
+        path = env.get("PATH_INNFO", "/")
+
+        #("/ctime", show_ctime)
+        for url,handler in self.urls:
+            if path == url:
+                return handler(env, start_response)
+
+        # 代表未找到路由信息，返回404错误
+        status = "404 Not Found!"
+        headers = []
+        start_response(status, headers)
+        return "not found!!"
+
+
+def show_ctime(env, start_response):
+    status = "200 OK"
+    headers = [
+        ("Conntent-Type", "text/plain")
+    ]
+    start_response(status, headers)
+    return time.ctime()
+
+def say_hello(env, start_response):
+    status = "200 OK"
+    headers = [
+        ("Conntent-Type", "text/plain")
+    ]
+    start_response(status, headers)
+    return "say_hello!!!!"
+
+
+if __name__ == "__main__":
+
+
+    urls = {
+        ("/ctime", show_ctime),
+        ("/sayhello", say_hello),
+    }
+
+    app = Application(urls)
+    http_server = HttpServer(app)
+    http_server.bind(8000)
+    http_server.start()
