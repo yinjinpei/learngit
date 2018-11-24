ab6e0b74228150bbf47dee3abf06a50e618717ad
peter <455758882@qq.com>
2018-11-24 23:21:17 +0800

    练习题5.py

--- 练习题5.py
+++ 练习题5.py
@@ -1,11 +1,20 @@
 
+#
+# import time
+# print(time.ctime())
+# print(time.time())
+#
+# a = max(1,2,4)
+# print(a)
+#
+# b = min(1.231,23,124,124,12,432,2,4,32)
+# print(b)
+#
 
-import time
-print(time.ctime())
-print(time.time())
 
-a = max(1,2,4)
-print(a)
-
-b = min(1.231,23,124,124,12,432,2,4,32)
-print(b)
+import datetime
+now1 = datetime.datetime.now()
+now2 = input("请输入一个2018-11-21格式的时间")
+now2 = datetime.datetime.strptime(now2,'%Y-%m-%d')
+now3 = now2+datetime.timedelta(days=1000)
+print(now3)
