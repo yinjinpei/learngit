701e0b731af3f8456eb670304796210e1a2874ca
peter <455758882@qq.com>
2018-12-09 19:12:46 +0800

    graphviz.py

--- python学习/第3章-数据结构与算法/第5节-排序与搜索/graphviz.py
+++ python学习/第3章-数据结构与算法/第5节-排序与搜索/graphviz.py
@@ -1,47 +1,62 @@
 # coding:utf-8
 # author:YJ沛
 
+#
+# def main():
+#     from graphviz import Digraph
+#
+#     dot = Digraph(comment='The Test Table')
+#
+#     # 添加圆点A,A的标签是Dot A
+#     dot.node('A', 'Dot A')
+#
+#     # 添加圆点 B, B的标签是Dot B
+#     dot.node('B', 'Dot B')
+#     # dot.view()
+#
+#     # 添加圆点 C, C的标签是Dot C
+#     dot.node('C', 'Dot C')
+#     # dot.view()
+#
+#     # 创建一堆边，即连接AB的两条边，连接AC的一条边。
+#     dot.edges(['AB', 'AC', 'AB'])
+#     # dot.view()
+#
+#     # 在创建两圆点之间创建一条边
+#     dot.edge('B', 'C', 'test')
+#     # dot.view()
+#
+#     # 获取DOT source源码的字符串形式
+#     print(dot.source)
+#     # // The Test Table
+#     # digraph {
+#     #   A [label="Dot A"]
+#     #   B [label="Dot B"]
+#     #   C [label="Dot C"]
+#     #   A -> B
+#     #   A -> C
+#     #   A -> B
+#     #   B -> C [label=test]
+#     # }
+#
+#     # 保存source到文件，并提供Graphviz引擎
+#     dot.render('test-output/test-table.gv', view=True)
+#
+#
+# if __name__ == "__main__":
+#     main()
 
-def main():
-    from graphviz import Digraph
+# C:\Program Files (x86)\Graphviz2.38
 
-    dot = Digraph(comment='The Test Table')
-
-    # 添加圆点A,A的标签是Dot A
-    dot.node('A', 'Dot A')
-
-    # 添加圆点 B, B的标签是Dot B
-    dot.node('B', 'Dot B')
-    # dot.view()
-
-    # 添加圆点 C, C的标签是Dot C
-    dot.node('C', 'Dot C')
-    # dot.view()
-
-    # 创建一堆边，即连接AB的两条边，连接AC的一条边。
-    dot.edges(['AB', 'AC', 'AB'])
-    # dot.view()
-
-    # 在创建两圆点之间创建一条边
-    dot.edge('B', 'C', 'test')
-    # dot.view()
-
-    # 获取DOT source源码的字符串形式
-    print(dot.source)
-    # // The Test Table
-    # digraph {
-    #   A [label="Dot A"]
-    #   B [label="Dot B"]
-    #   C [label="Dot C"]
-    #   A -> B
-    #   A -> C
-    #   A -> B
-    #   B -> C [label=test]
-    # }
-
-    # 保存source到文件，并提供Graphviz引擎
-    dot.render('test-output/test-table.gv', view=True)
+# coding:utf-8
+# from graphviz import Digraph
+#
+# g = Digraph('测试图片')
+# g.node(name='a',color='red')
+# g.node(name='b',color='blue')
+# g.edge('a','b',color='green')
+# g.view()
 
 
-if __name__ == "__main__":
-    main()
+import os
+print(os.environ["PATH"])
