f82b4f2ad110402700f673e89a5c4612d5d5509e
peter <455758882@qq.com>
2019-10-22 22:04:32 +0800

    03-Django-数据库设计

--- python学习/第6章-Django/DjangoProject/booktest/models.py
+++ python学习/第6章-Django/DjangoProject/booktest/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class BookInfo(models.Model):
+    '''
+    图书表结构设计：
+        表名：BookInfo
+        图书名称：btitle
+        图书发布时间：bpub_date
+    '''
+    btitle=models.CharField(max_length=20)
+    bpub_date=models.DateTimeField()
+
+class HeroInfo(models.Model):
+    '''
+    英雄表结构设计：
+        表名：HeroInfo
+        英雄姓名：hname
+        英雄性别：hgender
+        英雄简介：hcontent
+        所属图书：hbook
+    '''
+    hname=models.CharField(max_length=10)
+    hgender=models.BooleanField()
+    hcontent=models.CharField(max_length=1000)
+    hbook=models.ForeignKey(BookInfo,on_delete=models.CASCADE,)
+
