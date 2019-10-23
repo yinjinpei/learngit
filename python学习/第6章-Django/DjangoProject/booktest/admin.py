778e450218de9a92fd1d0f3d9b78d9f5e60e040c
peter <455758882@qq.com>
2019-10-23 21:19:36 +0800

    Django-自定义列表页属性

--- python学习/第6章-Django/DjangoProject/booktest/admin.py
+++ python学习/第6章-Django/DjangoProject/booktest/admin.py
@@ -4,5 +4,15 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(BookInfo)
-admin.site.register(HeroInfo)
+
+class BookInfoAdmin(admin.ModelAdmin):
+    list_display = ['id','btitle','bpub_date']
+
+
+class HeroInfoAdmin(admin.ModelAdmin):
+    list_display = ['id','hname','hgender','hcontent']
+
+
+admin.site.register(BookInfo,BookInfoAdmin)
+admin.site.register(HeroInfo,HeroInfoAdmin)
+
