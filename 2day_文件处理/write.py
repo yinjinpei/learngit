cb01b84ef058e20b18f1058d06715d9655f5f0fe
peter <ex_wljr_yinjinpei@pingan.com.cn>
2019-10-25 00:00:17 +0800

    write

--- 2day_文件处理/write.py
+++ 2day_文件处理/write.py
@@ -2,6 +2,13 @@
 #_*_ coding:utf-8 _*_
 
 
-a=[1,2,3]
-a.remove(4)
-print(a)
+f = open('write_passwdFile.txt','r')
+for i in f.readlines():
+    print(i)
+f.close()
+
+f = open("write_file.txt",'w')
+f.write('hihi')     #以覆盖形式写入
+f.write('sksk')
+f.write('\nggggg')  #\n 表示换行后再写入
+f.close()
