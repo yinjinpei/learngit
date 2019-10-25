08fe14d57f5062a8533e7517d389e0058c5aa8ed
peter <ex_wljr_yinjinpei@pingan.com.cn>
2019-10-25 17:22:22 +0800

    Django-传送数据

--- python学习/第6章-Django/DjangoProject/booktest/views.py
+++ python学习/第6章-Django/DjangoProject/booktest/views.py
@@ -3,13 +3,16 @@
 from django.http import *
 # from django.template import RequestContext,loader
 from django.shortcuts import render
+from .models import *
 
 def index(request):
     # 方法一
     # temp=loader.get_template('booktest/index.html')
     # return HttpResponse(temp.render())
     # 方法二
-    return render(request,'booktest/index.html')
+    bookList={'bookName':['大话西游','演员的自我修养','ptyhon3基础']}
+    # bookList={'bookName':'大话西游'}
+    return render(request,'booktest/index.html',bookList)
 
 def index_bak(request):
     return HttpResponse('<h1>您好,服务器正在维护中...</h1>')
