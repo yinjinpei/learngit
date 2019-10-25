2c74cbe2bc6132c7bb5f93add5b3029455935a20
peter <ex_wljr_yinjinpei@pingan.com.cn>
2019-10-25 16:14:22 +0800

    Django-使用render加载页面

--- python学习/第6章-Django/DjangoProject/booktest/views.py
+++ python学习/第6章-Django/DjangoProject/booktest/views.py
@@ -1,12 +1,15 @@
 # from django.shortcuts import render
 # Create your views here.
 from django.http import *
-from django.template import RequestContext,loader
+# from django.template import RequestContext,loader
+from django.shortcuts import render
 
 def index(request):
-    temp=loader.get_template('booktest/index.html')
-    return HttpResponse(temp.render())
-
+    # 方法一
+    # temp=loader.get_template('booktest/index.html')
+    # return HttpResponse(temp.render())
+    # 方法二
+    return render(request,'booktest/index.html')
 
 def index_bak(request):
     return HttpResponse('<h1>您好,服务器正在维护中...</h1>')
