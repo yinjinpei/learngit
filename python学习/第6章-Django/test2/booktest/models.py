d7f79e4bb110267ceefc3b59d5184cf1336fed74
peter <455758882@qq.com>
2019-10-26 16:37:11 +0800

    django-04_自定义管理器

--- python学习/第6章-Django/test2/booktest/models.py
+++ python学习/第6章-Django/test2/booktest/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+
+class BookInfoManager(models.Manager):
+    def get_queryset(self):
+        return super(BookInfoManager,self).get_queryset().filter(isDelete=False)
+
+
+class BookInfo(models.Model):
+    btitle=models.CharField(max_length=20)
+    bpub_date=models.DateTimeField(db_column='pub_date')
+    bread=models.IntegerField(default=0)
+    bcommet=models.IntegerField(null=False)
+    isDelete=models.BooleanField(default=False)
+    class Meta:
+        db_table='bookinfo'
+    books1=models.Manager()
+    books2 = BookInfoManager()
+
+class HeroInfo(models.Model):
+    hanme=models.CharField(max_length=10)
+    hgender=models.BooleanField(default=True)
+    hcontent=models.CharField(max_length=1000)
+    isDelete=models.BooleanField(default=False)
+    book=models.ForeignKey(BookInfo, on_delete=models.CASCADE)
+
