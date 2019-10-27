39b020e4071373b67577865122a82d7c60d311ae
peter <455758882@qq.com>
2019-10-27 23:40:35 +0800

    django-视图--17-POST获取传参

--- python学习/第6章-Django/test3/booktest/urls.py
+++ python学习/第6章-Django/test3/booktest/urls.py
@@ -15,16 +15,15 @@ urlpatterns = [
     # re_path('(\d+)',views.detail),
 
     re_path(r'^(\d+)/(\d+)/(\d+)/$',views.detail2),  #匹配 数字/数字/数字/ ，如:2019/10/27
-
     # re_path('(?P<p1>\d+)/(?P<p2>\d+)/(?P<p3>\d+)/',views.detail3), #关键字参数，指定参数位置，和上面一样
 
     re_path(r'^peter',views.peter),
 
-
     re_path(r'^getTest1/$',views.getTest1),
-
     re_path(r'^getTest2/$',views.getTest2),
-
     re_path(r'^getTest3/$',views.getTest3),
+
+    re_path(r'^postTest1/$',views.postTest1),
+    re_path(r'^postTest2/$',views.postTest2),
 
 ]
--- python学习/第6章-Django/test3/booktest/views.py
+++ python学习/第6章-Django/test3/booktest/views.py
@@ -29,11 +29,11 @@ def peter(request):
     # 属性:path：一个字符串，表示请求的页面的完整路径，不包含域名
     return HttpResponse('输出路径使用request.path:  &nbsp&nbsp'+request.path)
 
-# 展示链接的页面
+# get展示链接的页面
 def getTest1(request):
     return render(request,'booktest/getTest1.html')
 
-# 接收一键一值的情况
+# get方式，接收一键一值的情况
 def getTest2(request):
     # 获取传递过来的值，方法一：
     # a1=request.GET.get('a')
@@ -47,8 +47,30 @@ def getTest2(request):
     # 向模板中传递上下文，进行渲染
     return render(request,'booktest/getTest2.html',context)
 
-# 接收一键多值的情况
+# get方式，接收一键多值的情况
 def getTest3(request):
+    # 获取一键多个值,返回的是一个列表
     a3=request.GET.getlist('a')
     context={'a':a3}
     return render(request,'booktest/getTest3.html',context)
+
+
+# post方式，接收一键一值的情况
+def postTest1(request):
+    return render(request,'booktest/postTest1.html')
+
+# post方式，接收一键多值的情况
+def postTest2(request):
+    # 获取传递过来的单个值，方法一：
+    uname=request.POST.get('uname')
+
+    # 获取传递过来的单个值，方法二：推荐使用
+    upwd=request.POST['upwd']
+    ugender=request.POST['ugender']
+
+    # 获取一键多个值,返回的是一个列表
+    uhobby=request.POST.getlist('uhobby')
+
+    context={'uname':uname,'upwd':upwd,'ugender':ugender,'uhobby':uhobby}
+    return render(request,'booktest/postTest2.html',context)
+
