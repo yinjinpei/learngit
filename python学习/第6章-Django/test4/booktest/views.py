9e52ed5708f4ac0d6544d2a57322105ab2ebbdc4
peter <ex_wljr_yinjinpei@pingan.com.cn>
2019-11-08 17:33:11 +0800

    Django-test4-{%empty%}用法

--- python学习/第6章-Django/test4/booktest/views.py
+++ python学习/第6章-Django/test4/booktest/views.py
@@ -5,13 +5,16 @@ from .models import *
 
 
 def index(request):
-    # 练习一
+    # 练习一，获取主键为1的英雄
     # hero=HeroInfo.objects.get(pk=1)
     # hero2=HeroInfo.objects.get(hcontent='医术')
     # context={'hero':hero,'hero2':hero2}
 
-    # 练习二
-    list=HeroInfo.objects.all()
-    context={'list':list}
+    # 练习二，获取所有英雄
+    # list=HeroInfo.objects.all()
+    # context={'list':list}
 
+    # 练习三，获取被已删除的英雄
+    list2=HeroInfo.objects.filter(isDelete=True)
+    context={'list2':list2}
     return render(request, 'booktest/index.html',context)
