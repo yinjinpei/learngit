302fdf8f63e5162e465ba22d1b83c75939337f76
peter <455758882@qq.com>
2019-12-18 00:05:15 +0800

    新增日志

--- python学习/第6章-Django/startSoftware/software/views.py
+++ python学习/第6章-Django/startSoftware/software/views.py
@@ -3,8 +3,15 @@ import os
 from django.shortcuts import render
 from django.db.models import Max, F, Q
 from django.http import HttpResponse
-from win32 import win32api
+# from win32 import win32api
 from .models import *
+
+
+import logging
+# 生成一个以当前文件名为名字的logger实例
+logger = logging.getLogger(__name__)
+# 生成一个名为collect的logger实例
+collect_logger = logging.getLogger("collect")
 
 
 # 获取应用程序路径
@@ -14,6 +21,7 @@ def getAppDir(appName):
         if appName == app.appName:
             print('应用名：' + app.appName + '  路径：' + app.appDir)
             return app.appDir
+    logging.error(("ERROR：%s应用程序路径找不到！！！")%appName)
     return ("not found the path for \"%s\"") % appName
 
 
@@ -47,15 +55,18 @@ def index(request):
 #     win32api.ShellExecute(0, 'open', appDir, '', '', 1)  # 打开外部应用程序的另一种方法
 #     return index(request)
 
-# 打开应用程序
+# 用户打开应用请求处理
 def openAppHelper(request):
     if request.method=='GET':
         appName = request.GET.get('name')
         appDir = getAppDir(appName)
+        logging.info("打开应用程序："+appDir)
         flag = openApp(appDir)
         if flag:
             return index(request)
         else:
+            logging.error(("ERROR：%s应用程序找不到！！！") % appName)
             return HttpResponse(("not found the path for \"%s\"") % appName)
     else:
+        logging.error("ERROR：请求失败，请求方式不是GET！")
         return HttpResponse("请求失败！！")
--- python学习/第6章-Django/startSoftware/startSoftware/settings.py
+++ python学习/第6章-Django/startSoftware/startSoftware/settings.py
@@ -108,6 +108,56 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+BASE_LOG_DIR = os.path.join(BASE_DIR, "log")
+LOGGING = {
+    'version': 1,  # 保留字
+    'disable_existing_loggers': False,  # 禁用已经存在的logger实例
+    # 日志文件的格式
+    'formatters': {
+        # 详细的日志格式
+        'standard': {
+            "format":"%(asctime)s - %(message)s",
+        },
+    },
+    # 过滤器
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    # 处理器
+    'handlers': {
+        # # 在终端打印
+        # 'console': {
+        #     'level': 'DEBUG',
+        #     'filters': ['require_debug_true'],  # 只有在Django debug为True时才在屏幕打印日志
+        #     'class': 'logging.StreamHandler',  #
+        #     'formatter': 'standard'
+        # },
+        # 默认的
+        'default': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',  # 保存到文件，自动切
+            'filename': os.path.join(BASE_LOG_DIR, "message.log"),  # 日志文件
+            'maxBytes': 1024 * 1024 * 500,  # 日志大小 500M
+            'backupCount': 3,  # 最多备份几个
+            'formatter': 'standard',
+            'encoding': 'utf-8',
+        },
+
+    },
+    'loggers': {
+       # 默认的logger应用如下配置
+        '': {
+            # 'handlers': ['default', 'console'],  # 上线之后可以把'console'移除
+            'handlers': ['default'],  # 上线之后可以把'console'移除
+            'level': 'DEBUG',
+            'propagate': True,  # 向不向更高级别的logger传递
+        },
+
+    },
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
