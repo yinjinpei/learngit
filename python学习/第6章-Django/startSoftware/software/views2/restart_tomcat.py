8507bdfb47b32cb54e035819caac316c1ddb04ce
yinjinpei <455758882@qq.com>
2020-08-07 18:01:29 +0800

    优化

--- python学习/第6章-Django/startSoftware/software/views2/restart_tomcat.py
+++ python学习/第6章-Django/startSoftware/software/views2/restart_tomcat.py
@@ -59,105 +59,113 @@ def restart_tomcat(request):
 
         return render(request, 'software/restart_tomcat.html', locals())
 
-    print('这是一个好东西!')
     if not request.is_websocket():  # 判断是不是websocket连接
+        print('如果是普通的http方法')
         try:  # 如果是普通的http方法
             message = request.GET['message']
             return HttpResponse(message)
         except:
+            server_config = views.getConfig('config\\software_config\\shar_server_config.ini')
+            pafa5_serverIP = server_config.get_value('pafa5_serverIP','ip_list')
+            pafa3_serverIP = server_config.get_value('pafa3_serverIP','ip_list')
+            lotus_serverIP = server_config.get_value('lotus_serverIP','ip_list')
+
+            pafa5_ip_list=pafa5_serverIP.strip().split(',')
+            pafa3_ip_list=pafa3_serverIP.strip().split(',')
+            lotus_ip_list=pafa3_serverIP.strip().split(',')
+
+            message=pafa5_ip_list
             return render(request, 'software/restart_tomcat.html', locals())
     else:
         for message in request.websocket:
             message = message.decode('utf-8')  # 接收前端发来的数据
             print(message)
-            if message == 'backup_all':  # 这里根据web页面获取的值进行对应的操作
-                serverIP = request.POST.get('serverIP')
-                print(serverIP,'==================')
+            # if message == 'backup_all':  # 这里根据web页面获取的值进行对应的操作
 
-                command = 'sh /opt/test.sh'  # 这里是要执行的命令或者脚本
-                # cmds=['export LANG=zh_CN.UTF-8','echo $LANG','pwd','dir -lh','hostname -i','date']
-                cmds=['w','date','cat /opt/test.sh']
+            command = 'sh /opt/test.sh'  # 这里是要执行的命令或者脚本
+            # cmds=['export LANG=zh_CN.UTF-8','echo $LANG','pwd','dir -lh','hostname -i','date']
+            cmds=['tail -F /opt/test.log']
 
-                # 远程连接服务器
-                server_config=views.getConfig('config\\software_config\\restart_tomcat.ini')
-                hostname=server_config.get_value('shar_server_config','hostname')
-                username=server_config.get_value('shar_server_config','username')
-                password=server_config.get_value('shar_server_config','password')
-                print('hostname,username,password: ',hostname,username,password)
+            # 远程连接服务器
+            server_config=views.getConfig('config\\software_config\\shar_server_config.ini')
+            hostname=server_config.get_value('serverIP','hostname')
+            username=server_config.get_value('serverIP','username')
+            password=server_config.get_value('serverIP','password')
+            print('hostname,username,password: ',hostname,username,password)
 
-                ssh = paramiko.SSHClient()
-                ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                ssh.connect(hostname=hostname, username=username, password=password,timeout=60)
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(hostname=hostname, username=username, password=password,timeout=60)
 
-                # # 实例化一个transport对象
-                # transport = paramiko.Transport(hostname, 22)
-                # # 建立连接
-                # transport.connect(username=username, password=password)
-                # # 将sshclient的对象的transport指定为以上的transport
-                # ssh = paramiko.SSHClient()
-                # ssh._transport = transport
-                # for command in cmds:
-                #     # 执行命令，和传统方法一样
-                #     stdin, stdout, stderr = ssh.exec_command('cd /opt;./test.sh')
-                #     # print(stdout.read().decode())
-                #     while True:
-                #         nextline = stdout.readline().strip() # 读取脚本输出内容
-                #         request.websocket.send(nextline.encode('utf-8'))  # 发送消息到客户端
-                #         print(nextline)
-                #         # 判断消息为空时,退出循环
-                #         if not nextline:
-                #             print('判断消息为空时,退出循环')
-                #             break
-                # # 关闭连接
-                # transport.close()
-                # ssh.close()
-
-                print('激活连接的终端！')
-                channel = ssh.invoke_shell()
-                print('设置读、写操作超时时间')
-                channel.settimeout(10)
-                time.sleep(0.5)
-
-                print('发送命令行：%s' % cmds)
-                for command in cmds:
-                    channel.send(command + '\n')
-                    time.sleep(0.5)
-                    try:
-                        command_res = channel.recv(65533).decode('utf-8')
-                        print('---' * 30)
-                        print(type(command_res))
-                        print(command_res)
-                        print('===' * 30)
-                        request.websocket.send(command_res.encode('utf-8'))  # 发送消息到客户端
-                    except Exception as e:
-                        print('***' * 30)
-                        print(e)
-                        continue
-
-                channel.close()
-                ssh.close()
-
-            #     # 务必要加上get_pty=True,否则执行命令会没有权限
-            #     stdin, stdout, stderr = ssh.exec_command(command, get_pty=True)
-            #     # result = stdout.read()
-            #     print('stdin:',stdin)
-            #     print('stdout:',stdout)
-            #     print('stderr:',stderr)
-            #
-            #     # 循环发送消息给前端页面
-            #     print('循环发送消息给前端页面!!')
+            # # 实例化一个transport对象
+            # transport = paramiko.Transport(hostname, 22)
+            # # 建立连接
+            # transport.connect(username=username, password=password)
+            # # 将sshclient的对象的transport指定为以上的transport
+            # ssh = paramiko.SSHClient()
+            # ssh._transport = transport
+            # for command in cmds:
+            #     # 执行命令，和传统方法一样
+            #     stdin, stdout, stderr = ssh.exec_command('cd /opt;./test.sh')
+            #     # print(stdout.read().decode())
             #     while True:
-                #         nextline = stdout.readline().strip()  # 读取脚本输出内容
+            #         nextline = stdout.readline().strip() # 读取脚本输出内容
             #         request.websocket.send(nextline.encode('utf-8'))  # 发送消息到客户端
             #         print(nextline)
             #         # 判断消息为空时,退出循环
             #         if not nextline:
             #             print('判断消息为空时,退出循环')
             #             break
-            #     ssh.close()  # 关闭ssh连接
-            #     print('关闭ssh连接')
-            # else:
-            #     request.websocket.send('小样儿，没权限!!!'.encode('utf-8'))
+            # # 关闭连接
+            # transport.close()
+            # ssh.close()
+
+            print('激活连接的终端！')
+            channel = ssh.invoke_shell()
+            print('设置读、写操作超时时间')
+            channel.settimeout(10)
+            time.sleep(0.5)
+
+            print('发送命令行：%s' % cmds)
+            for command in cmds:
+                channel.send(command + '\n')
+                time.sleep(0.5)
+                try:
+                    command_res = channel.recv(65533).decode('utf-8')
+                    print('---' * 30)
+                    print(type(command_res))
+                    print(command_res)
+                    print('===' * 30)
+                    request.websocket.send(command_res.encode('utf-8'))  # 发送消息到客户端
+                except Exception as e:
+                    print('***' * 30)
+                    print(e)
+                    continue
+
+            channel.close()
+            ssh.close()
+
+        #     # 务必要加上get_pty=True,否则执行命令会没有权限
+        #     stdin, stdout, stderr = ssh.exec_command(command, get_pty=True)
+        #     # result = stdout.read()
+        #     print('stdin:',stdin)
+        #     print('stdout:',stdout)
+        #     print('stderr:',stderr)
+        #
+        #     # 循环发送消息给前端页面
+        #     print('循环发送消息给前端页面!!')
+        #     while True:
+            #         nextline = stdout.readline().strip()  # 读取脚本输出内容
+        #         request.websocket.send(nextline.encode('utf-8'))  # 发送消息到客户端
+        #         print(nextline)
+        #         # 判断消息为空时,退出循环
+        #         if not nextline:
+        #             print('判断消息为空时,退出循环')
+        #             break
+        #     ssh.close()  # 关闭ssh连接
+        #     print('关闭ssh连接')
+        # else:
+        #     request.websocket.send('小样儿，没权限!!!'.encode('utf-8'))
 
 
 def manager_login(request):
