b460f596deedb71911533dd05d97ea0f189172c6
peter <455758882@qq.com>
2020-10-19 22:36:58 +0800

    多线程

--- 练习题6.py
+++ 练习题6.py
@@ -1,8 +1,28 @@
-class A(object):
-    def show(self):
-        print('hi')
+from threading import Thread
+import time
+import random
+
+SR_list=[]
+def get_gitlab_SR():
+    global SR_list
+    time.sleep(random.randint(1,5))
+    SR_list.append(random.randint(1,100))
+    print('线程SR_list值：',SR_list)
 
 
-def B():
-    print('hello boy!')
+
+thread_list=[]
+for i in range(5):
+    t=Thread(target=get_gitlab_SR)
+    t.start()
+    print(t)
+    thread_list.append(t)
+
+
+for thread in thread_list:
+    thread.join()
+
+print('汇总：',SR_list)
+
+
 
