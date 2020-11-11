60d4cbd9a495b22af7694c5ab223fee9d47892ff
yinjinpei <455758882@qq.com>
2020-11-11 15:35:01 +0800

    修改时区

--- python学习/第6章-Django/startSoftware/startSoftware/settings.py
+++ python学习/第6章-Django/startSoftware/startSoftware/settings.py
@@ -166,13 +166,13 @@ LOGGING = {
 
 LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
