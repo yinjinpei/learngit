7763c20262d2126ff1ffd47924f634c60a9fe017
yinjinpei <455758882@qq.com>
2021-03-10 18:48:18 +0800

    pandas库使用

--- python学习/第1章-python基础/pandas库/02-pandas操作excel-写入.py
+++ python学习/第1章-python基础/pandas库/02-pandas操作excel-写入.py
@@ -7,5 +7,11 @@ from pandas import DataFrame
 dic1 = {'标题列1': ['张三','李四'],
         '标题列2': [80, 90]
        }
-df = pd.DataFrame(dic1)
-df.to_excel('1.xlsx', index=False)
+# df = pd.DataFrame(dic1)
+#
+# df.to_excel('1.xlsx', index=False)
+
+
+a='abcdef'
+a=a[:-1]
+print(a)
