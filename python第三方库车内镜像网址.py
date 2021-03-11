a5f9470367ba77d231d51445075601f847f6eaaa
peter <455758882@qq.com>
2021-03-11 23:37:07 +0800

    python第三方库车内镜像网址

---
+++ python第三方库车内镜像网址.py
@@ -0,0 +1,19 @@
+# coding:utf-8
+# author:YJ沛
+
+
+'''
+清华：https://pypi.tuna.tsinghua.edu.cn/simple
+
+阿里云：http://mirrors.aliyun.com/pypi/simple/
+
+中国科技大学：https://pypi.mirrors.ustc.edu.cn/simple/
+
+华中理工大学：http://pypi.hustunique.com/
+
+山东理工大学：http://pypi.sdutlinux.org/
+
+豆瓣：http://pypi.douban.com/simple/
+
+官方：https://pypi.python.org/simple
+'''
